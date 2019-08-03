d6be548a8c54dd0d4eec55b2122380c892fcada3
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-03 16:19:25 +0700

    Add function signup on AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(Request $request)
+    {
+      $this->validate($request,[
+        'username' => 'required|unique:users',
+        'email'    => 'required|unique:users',
+        'password' => 'required',
+      ]);
+
+      return User::create([
+        'username' => $request->json('username'),
+        'email'    => $request->json('email'),
+        'password' => bcrypt($request->json('password')),
+      ]);
+    }
 }
