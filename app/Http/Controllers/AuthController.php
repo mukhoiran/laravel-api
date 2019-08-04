e53b5124aad3db5663271c410e48768fa2bab549
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-04 10:02:33 +0700

    add function signin on AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use JWTAuth;
 use App\Models\User;
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -21,5 +23,28 @@ class AuthController extends Controller
         'email'    => $request->json('email'),
         'password' => bcrypt($request->json('password')),
       ]);
+    }
+
+    public function signin(Request $request)
+    {
+      $this->validate($request,[
+        'username' => 'required', 'password' => 'required',
+      ]);
+
+      // grab credentials from the request
+      $credentials = $request->only('username', 'password');
+
+      try {
+          // attempt to verify the credentials and create a token for the user
+          if (! $token = JWTAuth::attempt($credentials)) {
+              return response()->json(['error' => 'invalid_credentials'], 401);
+          }
+      } catch (JWTException $e) {
+          // something went wrong whilst attempting to encode the token
+          return response()->json(['error' => 'could_not_create_token'], 500);
+      }
+
+      // all good so return the token
+      return response()->json(compact('token'));
     }
 }
