b4985a7dd8269b2411a882b650f8d1f84c907928
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-04 17:39:47 +0700

    CREATE - add function create tutorial

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Models\Tutorial;
 use Illuminate\Http\Request;
 
 class TutorialController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+      $this->validate($request,[
+        'title' => 'required',
+        'body'  => 'required'
+      ]);
+
+      $tutorial = $request->user()->tutorials()->create([
+        'title' => $request->json('title'),
+        'slug'  => str_slug($request->json('title')),
+        'body'  => $request->json('body'),
+      ]);
+
+      return $tutorial;
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function tutorials()
+    {
+      return $this->hasMany('App\Models\Tutorial');
+    }
 }
