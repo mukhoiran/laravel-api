b3037c681a600a8845b1047cb3c89f2469dc7c4f
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-04 20:52:05 +0700

    routes post comment

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,13 @@ Route::group(['middleware' => ['api']], function(){
 
   Route::group(['middleware' => ['jwt.auth']], function(){
     Route::get('/profile', 'UserController@show');
+
+    //=========Tutorial
     Route::post('/tutorial', 'TutorialController@store');
     Route::put('/tutorial/{id}', 'TutorialController@update');
     Route::delete('/tutorial/{id}', 'TutorialController@destroy');
+
+    //=========Tutorial Comment
+    Route::post('/comment/{tut_id}', 'CommentController@store');
   });
 });
